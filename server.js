ffc29d1ff6377d9fe1c98149c9718c4a9fe2417d
Barry Blaha <barryblaha@gmail.com>
2018-03-28 14:57:30 -0700

    wu-27: pulling node counts from the spreadsheet into the calculator

--- server.js
+++ server.js
@@ -1,12 +1,21 @@
-var express = require('express');
+const express = require('express');
+const bodyParser = require('body-parser');
 
 var app = express();
 var PORT = process.env.PORT || 1500;
 
+
 require('./routes/main-route.js');
 
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.text());
+app.use(bodyParser.json({ type: "application/vnd.api+json" }));
 app.use(express.static('public'));
+
+require('./api/api')(app);
 
 app.listen(PORT, function() {
     console.log('App listening on port: ' + PORT);
 });
+
